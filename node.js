00d73b793f9a9be44df94b8aaef1d5655210e89f
Gurmehar Singh <gurmehar@gmail.com>
2022-12-08 02:08:49 -0500

    initial commit

---
+++ node.js
@@ -0,0 +1,15 @@
+const express = require('express')
+var fs = require('fs');
+const {URLSearchParams} = require('url')
+
+const app = express()
+const port = 80
+const host = '0.0.0.0'
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+	res.sendFile("public/templates/main.html", {root: __dirname})
+})
+
+app.listen(port, host)
